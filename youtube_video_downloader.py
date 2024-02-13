c29936a70380bc7e8d6981c7534e373d6912cd84
demarillacizere <izered3@gmail.com>
2024-02-14 00:36:24 +0200

    Downloading high quality videos from youtube

---
+++ youtube_video_downloader.py
@@ -0,0 +1,9 @@
+'''
+Downloading youtube videos
+'''
+import pytube
+
+link = input('Youtube Video URL: ')
+video_download = pytube.YouTube(link)
+video_download.streams.first().download()
+print('Video Downloaded', link)
